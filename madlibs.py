234e8726f362689769212f0cd47a07391bd244df
RSalcido2019 <ricardo.salcido@outlook.com>
2022-03-05 16:47:16 -0600

    Concatenation

---
+++ madlibs.py
@@ -0,0 +1,8 @@
+# string concatenation (aka how to put strings together)
+
+Engineer = "Ricardo Salcido"
+
+# a few ways to concatenate
+print("my name is " + Engineer)
+print("my name is  {}" .format(Engineer))
+print(f"my name is  {Engineer}")
