02d598e7426286c7527839e80d4769455dc31f80
RSalcido2019 <ricardo.salcido@outlook.com>
2022-03-05 16:52:25 -0600

    Concatenation

--- madlibs.py
+++ madlibs.py
@@ -1,8 +1,10 @@
 # string concatenation (aka how to put strings together)
 
-Engineer = "Ricardo Salcido"
+# Engineer = "Ricardo Salcido"
 
 # a few ways to concatenate
-print("my name is " + Engineer)
-print("my name is  {}" .format(Engineer))
-print(f"my name is  {Engineer}")
+# print("my name is " + Engineer)
+# print("my name is  {}" .format(Engineer))
+# print(f"my name is  {Engineer}")
+
+  adj = input("Adjective: ")
