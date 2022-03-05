d5938bb4769bce1af2a3397a71dd09f2d0e14c14
RSalcido2019 <ricardo.salcido@outlook.com>
2022-03-05 17:01:39 -0600

    Concatenation of madlib  complete

--- madlibs.py
+++ madlibs.py
@@ -8,10 +8,12 @@
 # print(f"my name is  {Engineer}")
 
 
-  adj = input("Adjective: ")
-  verb1 = input("Verb: ")
-  verb2 = input("Verb: ")
-  famous_person =  input("Famous Person: ")
+adj = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+famous_person =  input("Famous Person: ")
 
-  madlib = f"Computer programming is do {adj}! It makes me so excited all the time because \
-   I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}!"
+madlib = f"Computer programming is so {adj}! It makes me so excited all the time because \
+I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}!"
+
+print(madlib)
