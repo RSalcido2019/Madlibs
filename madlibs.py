9f24b00c515dd5cd1443c2ff293a40a6a16d5510
RSalcido2019 <ricardo.salcido@outlook.com>
2022-03-05 16:57:50 -0600

    Concatenation of madlib

--- madlibs.py
+++ madlibs.py
@@ -9,6 +9,9 @@
 
 
   adj = input("Adjective: ")
+  verb1 = input("Verb: ")
+  verb2 = input("Verb: ")
+  famous_person =  input("Famous Person: ")
 
   madlib = f"Computer programming is do {adj}! It makes me so excited all the time because \
    I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}!"
